4d121f9247adb17df7347a59d74ffd282287b33c
Hey <hey825@qq.com>
2024-11-02 10:08:40 +0800

    refactor: add cli struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// rcli csv -i input.csv -o output.json --header -d ','
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "RCli")]
+#[command(version = "1.0")]
+#[command(about = "A simple to use, efficient, and full-featured command line tool", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Csv {
+        #[arg(short, long)]
+        input: Option<String>,
+        #[arg(short, long)]
+        output: Option<String>,
+        #[arg(short, long, default_value_t = false)]
+        header: bool,
+        #[arg(short, long, default_value_t = String::from(","))]
+        delimiter: String,
+    },
+}
+
 fn main() {
-    println!("Hello, rust!");
+    let cli = Cli::parse();
+    println!("{:?}", cli.command);
 }
