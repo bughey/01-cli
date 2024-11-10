24236e4da2ac002240f16c3e38e6d4617f1624bb
Hey <hey825@qq.com>
2024-11-10 22:34:23 +0800

    refactor: interface difinition implement

--- src/process/text.rs
+++ src/process/text.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::Read};
+use std::{fs, io::Read, path::Path};
 
 use anyhow::Result;
 use base64::prelude::*;
@@ -9,79 +9,63 @@ use crate::{
     utils::get_reader,
 };
 
-trait TextSign {
+pub trait TextSign {
     fn sign(&self, reader: &mut dyn Read) -> Result<Vec<u8>>;
 }
 
-trait TextVerify {
+pub trait TextVerify {
     fn verify(&self, reader: impl Read, sig: &[u8]) -> Result<bool>;
 }
 
-struct Blake3 {
+pub trait KeyLoader {
+    fn load(path: impl AsRef<Path>) -> Result<Self>
+    where
+        Self: Sized;
+}
+
+pub struct Blake3 {
     key: [u8; 32],
 }
 
-#[allow(dead_code)]
-struct Ed25519Signer {
+pub struct Ed25519Signer {
     key: SigningKey,
 }
 
-#[allow(dead_code)]
-struct Ed25519Verifier {
+pub struct Ed25519Verifier {
     key: VerifyingKey,
 }
 
-pub fn process_text(subcmd: TextSubCommand) -> Result<()> {
-    match subcmd {
-        TextSubCommand::Sign(opts) => process_sign(opts),
-        TextSubCommand::Verify(opts) => process_verify(opts),
+impl Blake3 {
+    pub fn new(key: [u8; 32]) -> Self {
+        Self { key }
+    }
+
+    pub fn try_new(key: &[u8]) -> Result<Self> {
+        let key = key.try_into().unwrap();
+        Ok(Self::new(key))
     }
 }
 
-fn process_sign(opts: TextSignOpts) -> Result<()> {
-    println!("{:?}", opts);
-
-    let mut reader = get_reader(opts.input.as_str())?;
-
-    let signed = match opts.format {
-        TextSignFormat::Blake3 => {
-            let key = fs::read(opts.key.as_str())?;
-            let key = key.try_into().unwrap();
-            let signer = Blake3 { key };
-            signer.sign(&mut reader)?
-        }
-        TextSignFormat::Ed25519 => {
-            // sign with ed25519
-            todo!()
-        }
-    };
-
-    let signed = BASE64_URL_SAFE_NO_PAD.encode(&signed);
-    println!("\nsigned: {}", signed);
-    Ok(())
-}
-
-fn process_verify(opts: TextVerifyOpts) -> Result<()> {
-    println!("{:?}", opts);
-
-    let reader = get_reader(opts.input.as_str())?;
-
-    match opts.format {
-        TextSignFormat::Blake3 => {
-            let key = fs::read(opts.key.as_str())?;
-            let key = key.try_into().unwrap();
-            let verifier = Blake3 { key };
-            let sig = BASE64_URL_SAFE_NO_PAD.decode(opts.sig.as_bytes())?;
-            let verified = verifier.verify(reader, &sig)?;
-            println!("\nverified: {}", verified);
-        }
-        TextSignFormat::Ed25519 => {
-            // verify with ed25519
-            todo!()
-        }
+impl Ed25519Signer {
+    pub fn new(key: SigningKey) -> Self {
+        Self { key }
     }
 
-    Ok(())
+    pub fn try_new(key: &[u8]) -> Result<Self> {
+        let key = SigningKey::from_bytes(key.try_into()?);
+        Ok(Self::new(key))
+    }
+}
+
+impl Ed25519Verifier {
+    pub fn new(key: VerifyingKey) -> Self {
+        Self { key }
+    }
+
+    pub fn try_new(key: &[u8]) -> Result<Self> {
+        let key = VerifyingKey::from_bytes(key.try_into()?)?;
+        Ok(Self::new(key))
+    }
 }
 
 impl TextSign for Blake3 {
@@ -100,12 +84,26 @@ impl TextVerify for Blake3 {
     }
 }
 
+impl KeyLoader for Blake3 {
+    fn load(path: impl AsRef<Path>) -> Result<Self> {
+        let key = fs::read(path)?;
+        Self::try_new(&key)
+    }
+}
+
 impl TextSign for Ed25519Signer {
     fn sign(&self, reader: &mut dyn Read) -> Result<Vec<u8>> {
         let mut buf = Vec::new();
         reader.read_to_end(&mut buf)?;
         let sig = self.key.sign(&buf);
         Ok(sig.to_bytes().to_vec())
+    }
+}
+
+impl KeyLoader for Ed25519Signer {
+    fn load(path: impl AsRef<Path>) -> Result<Self> {
+        let key = fs::read(path)?;
+        Self::try_new(&key)
     }
 }
 
@@ -117,4 +115,60 @@ impl TextVerify for Ed25519Verifier {
         let ret = self.key.verify(&buf, &sig).is_ok();
         Ok(ret)
     }
+}
+
+impl KeyLoader for Ed25519Verifier {
+    fn load(path: impl AsRef<Path>) -> Result<Self> {
+        let key = fs::read(path)?;
+        Self::try_new(&key)
+    }
+}
+
+pub fn process_text(subcmd: TextSubCommand) -> Result<()> {
+    match subcmd {
+        TextSubCommand::Sign(opts) => process_sign(opts),
+        TextSubCommand::Verify(opts) => process_verify(opts),
+    }
+}
+
+fn process_sign(opts: TextSignOpts) -> Result<()> {
+    println!("{:?}", opts);
+
+    let mut reader = get_reader(opts.input.as_str())?;
+
+    let signed = match opts.format {
+        TextSignFormat::Blake3 => {
+            let signer = Blake3::load(opts.key)?;
+            signer.sign(&mut reader)?
+        }
+        TextSignFormat::Ed25519 => {
+            let signer = Ed25519Signer::load(opts.key)?;
+            signer.sign(&mut reader)?
+        }
+    };
+
+    let signed = BASE64_URL_SAFE_NO_PAD.encode(&signed);
+    println!("\nsigned: {}", signed);
+    Ok(())
+}
+
+fn process_verify(opts: TextVerifyOpts) -> Result<()> {
+    println!("{:?}", opts);
+
+    let reader = get_reader(opts.input.as_str())?;
+
+    let sig = BASE64_URL_SAFE_NO_PAD.decode(opts.sig.as_bytes())?;
+    let verified = match opts.format {
+        TextSignFormat::Blake3 => {
+            let verifier = Blake3::load(opts.key)?;
+            verifier.verify(reader, &sig)?
+        }
+        TextSignFormat::Ed25519 => {
+            let verifier = Ed25519Verifier::load(opts.key)?;
+            verifier.verify(reader, &sig)?
+        }
+    };
+    println!("\nverified: {}", verified);
+
+    Ok(())
 }
